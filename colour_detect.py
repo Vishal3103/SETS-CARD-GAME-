62fcef7ed0697b57a449897f8c10f613d49abfdf
kbhandari98 <kbhandari98@gmail.com>
2020-10-03 14:24:31 +0530

    Update colour_detect.py

--- colour_detect.py
+++ colour_detect.py
@@ -4,7 +4,7 @@ import numpy as np
 im = cv2.imread('sets2.jpg')
 
 Mat mask
-// specify the range of colours that you want to include, you can play with the borders here
+// specify the range of colours that you want to include, you can play with the borders here if you like
 cv::Scalar lowerb = cv::Scalar(255,127,39)
 cv::Scalar upperb = cv::Scalar(255,127,39)
 
